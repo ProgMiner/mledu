9b827a1994a20bf193f5418889d32cb77e02decc
Yungc0de <evgeniigutin@gmail.com>
2020-02-20 01:49:53 +0300

    changed host

--- ml_server/settings.py
+++ ml_server/settings.py
@@ -118,5 +118,5 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
-STATIC_URL = ''
+STATIC_URL = '/static/'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
