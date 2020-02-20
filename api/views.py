3bd40e773cf4bb02791ae9684ab41b58a2d60510
Yungc0de <evgeniigutin@gmail.com>
2020-02-20 05:52:30 +0300

    test ml

--- api/views.py
+++ api/views.py
@@ -2,14 +2,22 @@ from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from ml import test
+from ml.test import Test
 
 
 class GetAnswer(APIView):
+
     def get(self, request):
-        data = test.data
+        data = Test.test_alg()
         return Response({"data": data})
 
     def post(self, request):
         data = request.data.get('data')
+        GetAnswer.save(data)
         return Response({"success": "nice"})
+
+    @staticmethod
+    def save(data):
+        Test.data = data
+
 # Create your views here.
--- ml/test.py
+++ ml/test.py
@@ -1,9 +1,17 @@
 from sklearn.datasets import load_iris
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.model_selection import cross_val_score
-X, y = load_iris(return_X_y=True)
-clf = DecisionTreeClassifier(random_state=42)
-data = cross_val_score(clf, X, y, cv=5)
-print(data)
+
+
+class Test:
+    def __init__(self):
+        self.data = 42
+
+    def test_alg(self):
+        X, y = load_iris(return_X_y=True)
+        clf = DecisionTreeClassifier(random_state=self.data)
+        data = cross_val_score(clf, X, y, cv=5)
+        return data
+
 
 
